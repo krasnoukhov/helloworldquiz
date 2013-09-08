97e32efc5f10e9d21b32ccce565669aafe00b0d0
darianiff <darianiff@gmail.com>
2013-09-08 18:32:19 +0300

    Update main.go
    twitter caption and description of the game upd.

--- controllers/main.go
+++ controllers/main.go
@@ -10,8 +10,8 @@ type MainController struct {
 
 func (this *MainController) Get() {
   this.Data["title"] = "Hello World Quiz"
-  this.Data["caption"] = "Programming Languages 'Hello World' Quiz"
-  this.Data["description"] = "Recognize programming language by 'Hello World' code snippet"
+  this.Data["caption"] = "Guess programming language by „Hello, world!“ snippet"
+  this.Data["description"] = "Guess programming language by „Hello, world!“ snippet"
   this.Data["host"] = this.Ctx.Request.Host
   this.TplNames = "index.tpl"
 }
